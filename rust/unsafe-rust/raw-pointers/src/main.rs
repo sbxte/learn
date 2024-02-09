1cb5136324caac9f06cc17ff7172ba9e77498e30
SByte <72693226+StormyBytes@users.noreply.github.com>
2024-02-09 14:55:51 +0700

    Unsafe rust: Raw pointers

---
+++ rust/unsafe-rust/raw-pointers/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+	// Using unsafe to break borrow checker rules :)
+
+	let mut num = 5;
+
+	let r1 = &num as *const i32;
+	let r2 = &mut num as *mut i32;
+
+	unsafe { // Dereferencing raw pointers
+		println!("r1 is {}", *r1);
+		println!("r2 is {}", *r2);
+	}
+
+	unsafe { // Mutating pointer contents
+		*r2 = 10;
+		println!("r1 is {}", *r1);
+		println!("r2 is {}", *r2);
+	}
+}
+
