78ce31f1467f589109d958df620736ca2145213a
SByte <72693226+StormyBytes@users.noreply.github.com>
2024-02-12 19:32:21 +0700

    Rust: Pell-number PellGenerator

--- rust/pell-number/src/main.rs
+++ rust/pell-number/src/main.rs
@@ -2,13 +2,13 @@ use once_cell::sync::Lazy;
 
 static mut PELL_VALUES: Lazy<Vec<u128>> = Lazy::new(|| vec![0, 1]);
 
-fn pell(n: u32) -> u128 {
+fn pell_mut_static(n: u32) -> u128 {
     unsafe { PELL_VALUES.get(n as usize) }
         // unsafe { PELL_VALUES }
         //     .get(n as usize)
         .map(|v| *v)
         .unwrap_or_else(|| {
-            let val: u128 = 2 * pell(n - 1) + pell(n - 2);
+            let val: u128 = 2 * pell_mut_static(n - 1) + pell_mut_static(n - 2);
             unsafe { PELL_VALUES.push(val) }
             val
         })
@@ -21,8 +21,27 @@ fn pell_recurse(n: u32) -> u128 {
     pell_recurse(n - 2) + 2 * pell_recurse(n - 1)
 }
 
+struct PellGenerator {
+    values: Vec<u128>,
+}
+
+impl PellGenerator {
+    fn new() -> Self {
+        Self { values: vec![0, 1] }
+    }
+
+    fn get(&mut self, n: u32) -> u128 {
+        self.values.get(n as usize).map(|v| *v).unwrap_or_else(|| {
+            let val = self.get(n - 2) + 2 * self.get(n - 1);
+            self.values.push(val);
+            val
+        })
+    }
+}
+
 fn main() {
+    let mut gen = PellGenerator::new();
     for i in 0..10 {
-        println!("Pell {}", pell(i),);
+        println!("Pell {}", gen.get(i));
     }
 }
