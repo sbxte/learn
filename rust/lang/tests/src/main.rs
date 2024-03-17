529610d376e36756be2c4aec8616a37c4a0e7222
SByte <72693226+StormyBytes@users.noreply.github.com>
2024-03-18 06:15:39 +0700

    Rust: cargo test

---
+++ rust/lang/tests/src/main.rs
@@ -0,0 +1,26 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn bad_add(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn good_add_test() {
+        assert_eq!(add(2, 3), 5);
+    }
+
+    #[test]
+    fn bad_add_test() {
+        assert_eq!(bad_add(2, 3), 5);
+    }
+}
+
+fn main() {
+    println!("Run cargo test!");
+}
