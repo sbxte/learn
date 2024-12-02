1bbe23008d25e4e195985e0a8404d61be7616e60
SByte <stormybytes@gmail.com>
2024-12-02 20:23:26 +0700

    Reorganize AOC 2024

--- rust/aoc/2024/day2/src/main.rs
+++ rust/aoc/2024/day2/src/main.rs
@@ -98,6 +98,12 @@ pub fn part2(input: &str) -> u32 {
     sum
 }
 
+fn main() {
+    let input = include_str!("input.txt");
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
 #[cfg(test)]
 mod d2 {
     use super::*;
