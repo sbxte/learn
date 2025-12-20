ae1a128cde5846617ab789b1a7fc13100754a13d
SByte <stormybytes@gmail.com>
2025-12-21 01:44:32 +0700

    rust proc macro attributes

--- rust/lang/macros/procmacro_derive/src/lib.rs
+++ rust/lang/macros/procmacro_derive/src/lib.rs
@@ -22,3 +22,37 @@ fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
     };
     gen.into()
 }
+
+#[proc_macro_attribute]
+pub fn hello_macro_attr(input: TokenStream, annotated_item: TokenStream) -> TokenStream {
+    assert!(input.is_empty()); // We dont want anything
+
+    println!("Where does this go?");
+
+    let item: syn::Item = syn::parse(annotated_item).unwrap();
+    let mut ast: syn::ItemFn = match item {
+        syn::Item::Fn(x) => x,
+        _ => panic!("Not a fn!"),
+    };
+
+    let hello: syn::Stmt = syn::parse(
+        quote! {
+            println!("Hello from hello_macro attribute!");
+        }
+        .into(),
+    )
+    .unwrap();
+    ast.block.stmts.insert(0, hello.into());
+
+    let bye: syn::Stmt = syn::parse(
+        quote! {
+            println!("Goodbye from hello_macro attribute!");
+        }
+        .into(),
+    )
+    .unwrap();
+    ast.block.stmts.push(bye.into());
+
+    use quote::ToTokens;
+    ast.into_token_stream().into()
+}
--- rust/lang/macros/src/main.rs
+++ rust/lang/macros/src/main.rs
@@ -1,9 +1,16 @@
 use procmacro::HelloMacro;
-use procmacro_derive::HelloMacro;
+use procmacro_derive::{HelloMacro, hello_macro_attr};
 
 #[derive(HelloMacro)]
 struct Pancakes;
 
+#[hello_macro_attr]
+fn halo() {
+    println!("Hallo");
+}
+
 fn main() {
     Pancakes::hello_macro();
+    halo();
 }
+
